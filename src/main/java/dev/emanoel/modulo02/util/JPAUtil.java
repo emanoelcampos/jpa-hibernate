eb051db7098c866d8b738267fde39ac46aca4832
emanoelcampos <emanoel.cv@gmail.com>
2023-08-18 00:45:27 -0300

    classe util para factory de conexão com o banco de dados

---
+++ src/main/java/dev/emanoel/modulo02/util/JPAUtil.java
@@ -0,0 +1,14 @@
+package dev.emanoel.modulo02.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+
+    private static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("loja");
+
+    public static EntityManager getEntityManager() {
+        return ENTITY_MANAGER_FACTORY.createEntityManager();
+    }
+}
